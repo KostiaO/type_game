b5ae779397dcc55596ddde304e179c343f06f1d8
unknown <avicostov2@gmail.com>
2022-07-16 15:43:24 +0200

    type-game first template

---
+++ game.js
@@ -0,0 +1,64 @@
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d');
+
+const userInput = document.getElementById('user-input');
+
+const containArr = ['type', 'faster', 'go', 'cheese', 'google'];
+
+const check = document.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    if (e.target.value === word.contain) {
+      word.containChange();
+      word.goOnTop();
+    }
+  }
+});
+
+const word = {
+  coords: {
+    x: 0,
+    y: 0
+  },
+
+  contain: 'First',
+
+  goDown (step) {
+    this.coords.y += step;
+  },
+
+  goOnTop() {
+    this.coords.y = 0;
+  },
+
+  containChange() {
+    this.contain = containArr[Math.floor(Math.random() * containArr.length)]
+  }
+};
+
+let timer;
+
+let stepCount = 0;
+
+let ticks = 0;
+
+timer = setInterval(game, 250);
+
+game();
+
+function game () {
+  ticks++;
+
+  ctx.clearRect(0, 0, 400, 200);
+
+  word.goDown(3);
+
+  ctx.font = '50px serif';
+
+  ctx.fillText(word.contain, word.coords.x, word.coords.y);
+
+  if (word.coords.y >= 200) {
+    word.containChange();
+    word.goOnTop();
+  }
+}
